377b359c77551a990cc30988828c9cc579130771
Shingo Ohtsuka <capriciouswalk@gmail.com>
2016-02-01 06:19:00 +0900

    ...

--- tarou.jirou.saburou/src/package.d
+++ tarou.jirou.saburou/src/package.d
@@ -6,4 +6,6 @@ public import samurai.Merits;
 public import samurai.Player;
 public import samurai.PlayerTarou;
 public import samurai.container.rbtree;
+public import samurai.container.util;
+public import samurai.meta;
 
