f2cde413e636fd356681afbf78d7aec6e1127321
Shingo Ohtsuka <capriciouswalk@gmail.com>
2017-01-12 15:06:43 +0900

    後手でロックオン時に身バレしてしまう可能性がある問題を修正

--- tarou.jirou.saburou/src/GameInfo.d
+++ tarou.jirou.saburou/src/GameInfo.d
@@ -790,7 +790,7 @@ class GameInfo {
       return false;
     }
     void findTarget() pure @safe nothrow {
-      if (this.side != 0 && !this.samuraiInfo[this.weapon].hidden) {
+      if (this.side != 0 && (isAttackContain || !this.samuraiInfo[this.weapon].hidden)) {
         return;
       }
       foreach (f; this.reservedTarget) {
