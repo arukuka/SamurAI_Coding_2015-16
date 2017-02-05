7c629951a503a37014445de2c57b774e6e91fa3f
Shingo Ohtsuka <capriciouswalk@gmail.com>
2017-02-05 09:38:01 +0900

    コマンドライン引数の仕様を変更

--- tarou.jirou.saburou/run.sh
+++ tarou.jirou.saburou/run.sh
@@ -1,3 +1,3 @@
 cd $(dirname $0)
 FilePath=`pwd`
-nice -20 ${FilePath}/main 114514 ${FilePath}/combo/107-0.txt ${FilePath}/combo/107-1.txt
+nice -20 ${FilePath}/main ${FilePath}/combo/107-0.txt ${FilePath}/combo/107-1.txt 114514
